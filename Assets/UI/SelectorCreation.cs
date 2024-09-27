c0da75b22aa9b4a34b85700c266a0bdf12341a0a
Jason Henderson <jasonalanhenderson@gmail.com>
2024-09-27 18:42:03 -0400

    UI Elements almost completed for playtest.
    Worked in on the UI elements, need to figure out a better way to draw the custom buttons, likely a beizer curve.

---
+++ Assets/UI/SelectorCreation.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+namespace radial
+{
+    public partial class RadialMenu : VisualElement
+    {
+        [UnityEngine.Scripting.Preserve]
+        public new class UxmlFactory : UxmlFactory<RadialMenu> { }
+
+        public RadialMenu()
+        {
+            generateVisualContent += GenerateVisualContent;
+        }
+
+        private void GenerateVisualContent(MeshGenerationContext context)
+        {
+            float width = contentRect.width;
+            float height = contentRect.height;
+
+            var painter = context.painter2D;
+            painter.BeginPath();
+            painter.lineWidth = 0f;
+            painter.Arc(new Vector2(width * 0.5f, height * 0.5f), width * 0.25f, 360f, 0f);
+            painter.ClosePath();
+            painter.fillColor = Color.white; ;
+            painter.Fill(FillRule.NonZero);
+            painter.Stroke();
+
+
+        }
+    }
+}

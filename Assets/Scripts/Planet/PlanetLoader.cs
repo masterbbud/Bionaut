595d1e31c3eaf9d9c0e10621ddd8a669a7137df2
Brandon Faunce <masterbbud@gmail.com>
2024-10-06 12:12:04 -0400

    Moved files around so it makes more sense

--- Assets/Scripts/Planet/PlanetLoader.cs
+++ Assets/Scripts/Planet/PlanetLoader.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+/*
+* Contains all static references for the planet.
+* This includes the planet size and prefabs such as particles
+* that need to get referenced by other classes.
+*/
 public class PlanetLoader : MonoBehaviour
 {
     public int[] loopAroundIgnoreLayers = new int[]{};
     public GameObject interactionParticlePrefab;
+    public static float planetHeight = 20;
+    public static float planetWidth = 30;
+    public float thisPlanetHeight;
+    public float thisPlanetWidth;
     void Awake()
     {
+        planetHeight = thisPlanetHeight;
+        planetWidth = thisPlanetWidth;
         InteractibleObject.particlePrefab = interactionParticlePrefab;
     }
     // Start is called before the first frame update

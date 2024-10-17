37822e82f918d6ff2e96ad24533f4e3037175d3b
Rylie <102985857+rylielovee@users.noreply.github.com>
2024-10-17 19:23:32 -0400

    Companion, and 3 critters added

---
+++ Assets/Scripts/Critters/SkiddishCritter.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SkiddishCritter : Critter
+{
+    [SerializeField]
+    float wanderTime, wanderRadius;
+
+    //[SerializeField]
+    //SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    float wanderWeight, fleeWeight, separationWeight, cohesionWeight, alignmentWeight;
+
+    [SerializeField]
+    float distance;  // max distance that critter will flee or seek 
+
+
+    void Start()
+    {
+        min = spriteRenderer.bounds.min;
+        max = spriteRenderer.bounds.max;
+    }
+
+    protected override Vector2 CalculateSteeringForces()
+    {
+        min = spriteRenderer.bounds.min;
+        max = spriteRenderer.bounds.max;
+
+        Vector2 wanderForce = Wander(wanderTime, wanderRadius) * wanderWeight;
+
+        Vector2 fleeForce = Flee(Player.main.transform.position) * fleeWeight;
+
+        Vector2 separationForce = Separation(CritterManager.critters) * separationWeight;
+
+        Vector2 cohesionForce = Cohesion(CritterManager.critters) * cohesionWeight;
+
+        Vector2 alignmentForce = Alignment(CritterManager.critters) * alignmentWeight;
+
+
+        if (Math.Abs(rb.velocity.x) > 0.1f)  // Prevent sprite jitter
+        {
+            if (rb.velocity.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+
+
+        if (Vector2.Distance(Player.main.transform.position, transform.position) < distance)
+        {
+            maxSpeed = 10;
+            return wanderForce + fleeForce + separationForce + cohesionForce + alignmentForce;
+        }
+        maxSpeed = 4;
+        return wanderForce + separationForce + cohesionForce + alignmentForce;
+
+
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        if (Vector2.Distance(Player.main.transform.position, transform.position) < distance)
+        {
+            Gizmos.DrawLine(Player.main.transform.position, transform.position);
+        }
+
+
+    }
+
+
+
+}
